798bbf13f451110fd443dbc91e44d542c856d62d
Agustin Valdes <42925417+dasgrossfuchs@users.noreply.github.com>
2021-05-30 20:10:17 -0700

    todo

--- examenU3/funciones.js
+++ examenU3/funciones.js
@@ -1,137 +1,41 @@
 module.exports = {
-    "procesar": procesar,
-    "CURP": CURP
+    "procesar": procesar
 }
 
-function procesar(temp) {
-    var RFC = PrimerL(temp.pname);
-    RFC += PrimerV(temp.pname);
-    RFC += PrimerL(temp.mname);
-    RFC += PrimerL(temp.fname);
-    RFC += temp.fecha.substring(2, 4);
-    RFC += temp.fecha.substring(5, 7);
-    RFC += temp.fecha.substring(8);
-    RFC = AltisonanteLooker(RFC);
-    return RFC.toUpperCase();
-}
-
-function CURP(temp) {
-    var curp = PrimerL(temp.pnameC.toUpperCase());
-    curp += PrimerV(temp.pnameC.toUpperCase());
-    curp += PrimerL(temp.mnameC.toUpperCase());
-    curp += PrimerL(temp.fnameC.toUpperCase());
-    curp += temp.fechaC.substring(2, 4);
-    curp += temp.fechaC.substring(5, 7);
-    curp += temp.fechaC.substring(8);
-    curp += temp.genC;
-    curp += estado(temp.edoC.toUpperCase());
-    curp += SegCons(temp.pnameC.toUpperCase());
-    curp += SegCons(temp.mnameC.toUpperCase());
-    curp += SegCons(temp.fnameC.toUpperCase());
-    curp = AltisonanteLooker(curp.toUpperCase());
-    curp += Homoclave(temp.fechaC);
-    return curp.toUpperCase();
-}
-
-function Homoclave(date) {
-    var result = '';
-    var chars = 'abcdefghijklmnopqrstuvwxyz';
-    if (date.substring(0, 4) <= '1999') {
-        result += Math.floor(Math.random() * 10);
-    } else { result += chars.charAt(Math.floor(Math.random() * chars.length)); }
-    result += Math.floor(Math.random() * 10);
-    return result.toUpperCase();
-}
-
-function SegCons(temp) {
-    if (temp == '') { return 'X'; }
-    const vocales = 'AEIOU';
-    for (let i = 1; i < temp.length; i++) {
-        if (!(vocales.includes(temp[i]))) {
-            return temp[i];
-        }
+function procesar(vals, TC,movimientos) {
+    var tipo = vals.tipoCambio;
+    var recibo = vals.RECIBIDO;
+    var pedido = vals.PEDIDO;
+    var total = 0.00;
+    var cambio = 0.00;
+    var mxn = 0;
+    var usd = 0;
+    if(tipo == 'COMPRA'){
+        total = (pedido) * TC.compra;
+        total = Math.round((total + Number.EPSILON) * 100) / 100;
+        cambio = (recibo - pedido);
+        cambio = Math.round((cambio + Number.EPSILON) * 100) / 100;
+        var mxn =  parseFloat(movimientos.mxn) - parseFloat(total);
+        var usd =  parseFloat(movimientos.usd) + parseFloat(pedido);
+        if ((recibo % 1) == 0) recibo = recibo + '.00';
+        if ((pedido % 1) == 0) pedido = pedido + '.00';
+        if ((cambio % 1) == 0) cambio = cambio + '.00';
+        if ((total % 1) == 0) total = total + '.00';
+        var transaccion = {pedido : pedido + ' MXN',recibo : recibo + ' MXN', total: total + ' USD', cambio: cambio+' MXN', tipo:tipo}
     }
-    return 'X';
-}
-
-
-function PrimerL(temp) {
-    if (temp == '') { return 'X'; }
-    if (temp.includes(' ')) {
-        const impName = NombreComp(temp);
-        temp = impName;
+    if(tipo == 'VENTA'){
+        total = (pedido) * TC.venta;
+        total = Math.round((total + Number.EPSILON) * 100) / 100;
+        cambio = (recibo - pedido);
+        cambio = Math.round((cambio + Number.EPSILON) * 100) / 100;
+        var mxn =  parseFloat(movimientos.mxn) - parseFloat(total);
+        var usd =  parseFloat(movimientos.usd) + parseFloat(pedido);
+        if ((recibo % 1) == 0) recibo = recibo + '.00';
+        if ((pedido % 1) == 0) pedido = pedido + '.00';
+        if ((cambio % 1) == 0) cambio = cambio + '.00';
+        if ((total % 1) == 0) total = total + '.00';
+        var transaccion = {pedido : pedido + ' USD',recibo : recibo + ' USD', total: total + ' MXN', cambio: cambio+' USD', tipo : tipo};
     }
-    var excep = "/^[!@#$%^&*()_+\-=\[\]{};':\"\\|,.<>\/?]*$/";
-    if (excep.indexOf(temp.charAt(0)) != -1) { return 'X'; }
-    if (temp.charAt(0) == 'Ñ') { return 'X'; }
-    return temp.charAt(0);
-}
-
-function PrimerV(temp) {
-    if (temp == '') { return 'X'; }
-    const vocales = 'AEIOU';
-    for (let i = 1; i < temp.length; i++) {
-        if (vocales.includes(temp[i])) {
-            return temp[i]
-        }
-        return 'X';
-    }
-}
-
-function NombreComp(temp) {
-    const names = [temp.split(' ')];
-    const names2 = [temp.split(' ')];
-    const excep1 = ['DA', 'DAS', 'DE', 'DEL', 'DER', 'DI', 'DIE', 'DD',
-        'EL', 'LA', 'LOS', 'LAS', 'LE', 'LES', 'MAC', 'MC', 'VAN', 'VON', 'Y'
-    ];
-    const excep2 = ['MARIA', 'MA.', 'MA', 'M', 'M.', 'JOSE', 'J', 'J.'];
-    for (let i = 0; i < temp.split(' ').length - 1; i++) {
-        if (excep1.includes(names[i])) {
-            const x = names2.indexOf(names[i]);
-            if (x == 0) { names2.splice(0, 1); }
-            names2.splice(x - 1, 2);
-        }
-    }
-    for (let i = 0; i < temp.split(' ').length - 1; i++) {
-        if (excep2.includes(names[i])) {
-            const x = names2.indexOf(names[i]);
-            names2.splice(x, 1);
-        }
-    }
-    return names[0];
-}
-
-function AltisonanteLooker(temp) {
-    const altisonantes = ['BACA', 'LOCO', 'BAKA', 'LOKA',
-        'BUEI', 'LOKO', 'BUEY', 'MAME', 'CACA', 'MAMO', 'CACO',
-        'MEAR', 'CAGA', 'MEAS', 'CAGO', 'MEON', 'CAKA', 'MIAR',
-        'CAKO', 'MION', 'COGE', 'MOCO', 'COGI', 'MOKO', 'COJA',
-        'MULA', 'COJE', 'MULO', 'COJI', 'NACA', 'COJO', 'NACO',
-        'COLA', 'PEDA', 'CULO', 'PEDO', 'FALO', 'PENE', 'FETO',
-        'PIPI', 'GETA', 'PITO', 'GUEI', 'POPO', 'GUEY', 'PUTA',
-        'JETA', 'PUTO', 'JOTO', 'QULO', 'KACA', 'RATA', 'KACO',
-        'ROBA', 'KAGA', 'ROBE', 'KAGO', 'ROBO', 'KAKA', 'RUIN',
-        'KAKO', 'SENO', 'KOGE', 'TETA', 'KOGI', 'VACA', 'KOJA',
-        'VAGA', 'KOJE', 'VAGO', 'KOJI', 'VAKA', 'KOJO', 'VUEI',
-        'KOLA', 'VUEY', 'KULO', 'WUEI', 'LILO', 'WUEY', 'LOCA'
-    ];
-    if (altisonantes.includes(temp.substring(0, 4))) {
-        return temp.substring(0, 1) + 'X' + temp.substring(2);
-    }
-    return temp;
-}
-
-function estado(temp) {
-    const edos = ['AGUASCALIENTES', 'BAJA CALIFORNIA', 'BAJA CALIFORNIA SUR',
-        'CAMPECHE', 'CHIAPAS', 'CHIHUAHUA', 'COAHUILA', 'COLIMA', 'CUIDAD DE MEXICO',
-        'DURANGO', 'GUANAJUATO', 'GUERRERO', 'HIDALGO', 'JALISCO', 'MEXICO', 'MICHOACAN',
-        'MORELOS', 'NAYARIT', 'NUEVO LEON', 'OAXACA', 'PUEBLA', 'QUERETARO', 'QUINTANA ROO',
-        'SAN LUIS POTOSI', 'SINALOA', 'SONORA', 'TABASCO', 'TAMAULIPAS', 'TLAXCALA', 'VERACRUZ',
-        'YUCATAN', 'ZACATECAS'
-    ];
-    const abv = ['AS', 'BC', 'BS', 'CC', 'CS', 'CH', 'CL', 'CM', 'DF', 'DG', 'GT', 'GR', 'HG',
-        'JC', 'MC', 'MN', 'MS', 'NT', 'NL', 'OC', 'PL', 'QO', 'QR', 'SP', 'SL', 'SR', 'TC', 'TS', 'TL',
-        'VZ', 'YN', 'ZS'
-    ];
-    if (edos.indexOf(temp) != -1) { return abv[edos.indexOf(temp)]; }
+    var resultado = {trans : transaccion, usd: usd, mxn:mxn };
+    return resultado;
 }
--- examenU3/routes/routes.js
+++ examenU3/routes/routes.js
@@ -8,71 +8,66 @@ const router = express.Router();
 //exportar nuestro modulo route
 module.exports = router;
 
-var mx = '<button name="switch-btn" class="btn btn-primary switch" type="submit"><img src="/img/mxflag.png">  <i class="fa fa-sync-alt"></i>  <img src="/img/usflag.png"></button>';
-var us = '<button name="switch-btn2" class="btn btn-primary switch" type="submit"><img src="/img/usflag.png">  <i class="fa fa-sync-alt"></i>  <img src="/img/mxflag.png"></button>';
-var chkMx = 'oninvalid = "this.setCustomValidity(\'VALOR DEBE SER DE 20 o MAS\')" min = "20" step="0.01"';
-var chkUs = 'oninvalid = "this.setCustomValidity(\'VALOR DEBE SER DE 1 o MAS\')" min = "1" step="0.01"';
+var mx = '<button name="1" class="btn btn-primary switch" type="submit"><img src="/img/mxflag.png">  <i class="fa fa-sync-alt"></i>  <img src="/img/usflag.png"><br> CAMBIAR TIPO DE TRANSACCIÓN</button>';
+var us = '<button class="btn btn-primary switch" type="submit"><img src="/img/usflag.png">  <i class="fa fa-sync-alt"></i>  <img src="/img/mxflag.png"><br> CAMBIAR TIPO DE TRANSACCIÓN</button>';
 var status = [
-    {btnSw : mx, L: 'MXN', R: 'USD', chk:chkMx ,btnSnd: 'Comprar Dólares'},
-    {btnSw : us, L: 'USD', R: 'MXN', chk:chkUs ,btnSnd: 'Vender Dólares'}
+    {btnSw : mx, L: 'MXN', R: 'USD' ,btnSnd: 'COMPRA DOLARES'},
+    {btnSw : us, L: 'USD', R: 'MXN' ,btnSnd: 'VENTA DOLARES'}
 ];
-var caja = {usdDIA:500000, usdCIERRE:500000, mxnDIA:500000, mxnCIERRE:500000};
+var caja = {usdDIA:9999.99, usdCIERRE:9999.99, mxnDIA:9999.99, mxnCIERRE:9999.99};
+var movimientos = {usd:0, mxn:0};
+var TC = {venta:19.92, compra:0.05};
 
-router.get('/', (req, res) => {
-    res.render('pages/home', { title: "Home", state :status[0]});
+router.get('/compraventa', (req, res) => {
+    res.render('pages/home', { title: "Transacción", state :status[0], caja, TC});
+});
+
+router.get('/ticket', (req, res) => {
+    res.redirect('/compraventa')
+});
+
+router.post('/compraventa', (req, res) => {
+    var temp = 0;
+    if (req.body.hasOwnProperty("1")) {
+        temp = 1;
+    } 
+    if (req.body.hasOwnProperty("save")) {
+        caja.mxnDIA = req.body.mxnCaja;
+        caja.mxnCIERRE = req.body.mxnCaja;
+        caja.usdDIA = req.body.usdCaja;
+        caja.usdCIERRE = req.body.usdCaja;
+        TC.compra = req.body.compraTc;
+        TC.venta = req.body.ventaTc;
+    }
+    if (req.body.hasOwnProperty("reset")) {
+        caja.mxnDIA = req.body.mxnCaja;
+        caja.mxnCIERRE = req.body.mxnCaja;
+        caja.usdDIA = req.body.usdCaja;
+        caja.usdCIERRE = req.body.usdCaja;
+        TC.compra = req.body.compraTc;
+        TC.venta = req.body.ventaTc;
+        var movimientos = {usd:0, mxn:0};
+    }
+    res.render('pages/home', { title: "Transacción", state : status[temp] , caja, TC});
+});
+
+router.post('/ticket', (req, res) => {
+    var total = funciones.procesar(req.body,TC,movimientos);
+    movimientos.mxn = parseFloat(total.mxn);
+    movimientos.usd = parseFloat(total.usd);
+    var temp = (parseFloat(caja.mxnDIA) + parseFloat(movimientos.mxn));
+    if ((temp % 1) == 0) temp = temp + '.00';
+    caja.mxnCIERRE = temp;
+    var temp = (parseFloat(caja.usdDIA) + parseFloat(movimientos.usd));
+    if ((temp % 1) == 0) temp = temp + '.00';
+    caja.usdCIERRE = temp; 
+    var transaccion = total.trans;
+    res.render('pages/ticket', { title: "Ticket de transacción", valores : transaccion });
 });
 
 router.get('/reporte', (req, res) => {
-    var transaccion = { peso:55,dolar:105,residuo:33,tipo:'COMPRA'};
-    res.render('pages/reporte', { title: "Reporte de transacción", valores : transaccion });
-    //res.render('pages/home', { title: "Home", state :status[0]});
+    res.render('pages/reporte', {title: "REPORTE DEL DIA", caja, movimientos});
 });
-
-router.post('/', (req, res) => {
-    if (req.body.hasOwnProperty("switch-btn")) {
-        var temp = 1;
-        res.render('pages/home', { title: "Home", state : status[temp] });
-    } 
-    if (req.body.hasOwnProperty("switch-btn2")) {
-        var temp = 0;
-        res.render('pages/home', { title: "Home", state : status[temp] });
-    }
-});
-
-router.post('/reporte', (req, res) => {
-    if(req.body.tipoCambio == 'COMPRA'){
-        var mxn = req.body.MXN;
-        var cambio = mxn%(1/0.05);
-        var cambio = Math.round((cambio + Number.EPSILON) * 100) / 100;
-        mxn = mxn - cambio;
-        var usd = mxn * 0.05;
-        var usd = Math.round((usd + Number.EPSILON) * 100) / 100
-        caja.mxnCIERRE += mxn;
-        caja.usdCIERRE -= usd;
-        var transaccion = { peso:mxn,dolar:usd,residuo:cambio,tipo:req.body.tipoCambio};
-        console.log(usd +'_'+ mxn)
-        res.render('pages/reporte', { title: "Reporte de transacción", valores : transaccion });
-    }
-    if(req.body.tipoCambio == 'VENTA'){
-        var usd = req.body.USD;
-        var cambio = usd % (1/19.92);
-        var cambio = Math.round((cambio + Number.EPSILON) * 100) / 100;
-        var usd = usd - cambio;
-        var mxn = usd * 19.92;
-        var mxn = Math.round((mxn + Number.EPSILON) * 100) / 100;
-        caja.usdCIERRE += usd;
-        caja.mxnCIERRE -= mxn;
-        var transaccion = { peso:mxn,dolar:usd,residuo:cambio, tipo:req.body.tipoCambio};
-        console.log(usd +'_'+ mxn)
-        res.render('pages/reporte', { title: "Reporte de transacción", valores : transaccion });
-    }
-});
-
-router.get('/about', (req, res) => {
-    res.render('pages/about', {title: "About"});
-});
-
-
-router.get('/contact', (req, res) => {
-    res.render('pages/contact', { title: "Contact"});
+router.get('/', (req, res) => {
+    res.render('pages/conf', {title: "REPORTE DEL DIA", caja, TC});
 });

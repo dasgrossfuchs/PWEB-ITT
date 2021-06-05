3bf0a24dac92fec78e585e3a3bb3152b611643fb
Agustin Valdes <42925417+dasgrossfuchs@users.noreply.github.com>
2021-06-04 21:14:38 -0700

    fix

--- CRUD/config.js
+++ CRUD/config.js
@@ -1,4 +1,5 @@
 module.exports={
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/mybdtest'
+    db: process.env.MONGODB || 'mongodb://localhost:27017/BdTest',
+    urlParser : { useNewUrlParser: true, useUnifiedTopology: true } 
 }
--- CRUD/index.js
+++ CRUD/index.js
@@ -1,18 +1,45 @@
 // npm install express express-handlebars mongoose
 // npm install nodemon --save-dev
-'use strict'
+'use strict';
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('./config');
-const app = express()
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
-mongoose.connect(config.db, (err,res)=>{
-    if(err){
-        console.log(`Error al conectar en la BD ${err}`)
-    }
-    console.log('Conexion a la BD exitosa')
-    app.listen(config.port, ()=>{
-        console.log(`Ejecutando en http://localhost:${config.port}`);
-    });
+const hbs = require('express-handlebars');
+const app = express();
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+//recursos estáticos/ públicos
+app.use('/static', express.static('public'))
+
+//motor de vistas
+
+app.engine('.hbs',hbs({
+    defaultLayout : 'index',
+    extname : '.hbs'
+}))
+
+app.set('view engine', '.hbs')
+
+app.get('/',(req,res)=>{
+    res.render('home');
+    // res.status(200).send('hola mundo')
 })
+//este va hasta el final para evitar problemas de conexión
+app.use((req,res)=>{
+    res.status(404).render('notfound');
+    console.log(res.statusCode);
+})
+
+mongoose.connect(config.db, config.urlParser, (err,res)=>{
+    if(err){
+        console.log(`Error al conectar en la BD ${err}`);
+    }
+    else  {
+        console.log('Conexión a la BD exitosa');
+        app.listen(config.port, ()=>{
+            console.log(`Ejecutando en http://localhost:${config.port}`)
+        });
+    }
+})
+//Agustin Valdés Fuchs
